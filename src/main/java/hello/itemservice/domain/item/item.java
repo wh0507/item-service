8e8d314953de0a7299812a05dcafada0c40ca5b9
wh0507 <banggu0507@gmail.com>
2022-04-10 16:52:25 +0900

    item instance

---
+++ src/main/java/hello/itemservice/domain/item/item.java
@@ -0,0 +1,18 @@
+package hello.itemservice.domain.item;
+
+public class item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public item() {
+    }
+
+    public item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}

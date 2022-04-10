417b52462b6f4ca9298487c2037e10e94714a02f
wh0507 <banggu0507@gmail.com>
2022-04-10 17:02:46 +0900

    ItemInstance

--- src/main/java/hello/itemservice/domain/item/item.java
+++ src/main/java/hello/itemservice/domain/item/item.java
@@ -3,17 +3,17 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 
 @Data
-public class item {
+public class Item {
 
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public item() {
+    public Item() {
     }
 
-    public item(String itemName, Integer price, Integer quantity) {
+    public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;

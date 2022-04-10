61ef5c9559595e15d93c7c3f7227900350de4983
wh0507 <banggu0507@gmail.com>
2022-04-10 18:58:38 +0900

    commit

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,10 @@
+package hello.itemservice.web.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic/items")
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+}
